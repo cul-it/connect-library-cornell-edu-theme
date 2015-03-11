da33dd5f105b844bb3c254eac998bca576124a35
Melissa Wallace <mhk33@cornell.edu>
2015-03-11 15:49:29 -0400

    new branding

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -28,9 +28,20 @@
   <div class="container">
     <div class="row">
       <div class="brand">
-        <a class="navbar-brand" href="/">CUL Social</a>
+        <a class="navbar-brand" href="/">Social@CUL</a>
       </div>
-      <div class="navigation">
+      <div class="social-types">
+        <ul class="nav nav-icons">
+          <li><a href="#"><i class="fa fa-pencil"></i></a></li>
+          <li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+          <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+          <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+          <li><a href="#"><i class="fa fa-youtube"></i></a></li>
+          <li><a href="#"><i class="fa fa-flickr"></i></a></li>
+        </ul>
+      </div>
+
+      <!-- <div class="navigation">
         <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
           <i class="fa fa-bars"></i> Menu
         </button>
@@ -38,13 +49,13 @@
           <div class="nav navbar-nav">
             <?php print render($page['navigation']); ?>
           </div>
-        </div><!--/.navbar-collapse-->
-      </div>
+        </div><
+      </div> -->
     </div>
   </div>
 </div>
 
-<div class="jumbotron">
+<!-- <div class="jumbotron">
   <div class="container">
     <div class="row">
       <div class="main-text">
@@ -69,7 +80,7 @@
       </div>
     </div>
   </div>
-</div>
+</div> -->
 
 <div class="social-feed">
   <div class="container-fluid">
