25395d5815feae896b718e6738d6864a0cb9deb7
Melissa Wallace <mhk33@cornell.edu>
2015-03-11 15:53:14 -0400

    add home page template

---
+++ templates/page--front.tpl.php
@@ -0,0 +1,59 @@
+<div id="skiptocontent"><a href="#maincontent">skip to main content</a></div>
+
+<div class="cornell-brand">
+  <div class="container hidden-xs">
+    <div class="row">
+      <div class="cornell-logo">
+        <div class="cu-logo">
+          <img src="/sites/all/themes/social/images/layout/cul-logo-white.gif" alt="Cornell University Library">
+          <a id="insignia-link" href="http://www.cornell.edu/">Cornell Insignia</a>
+          <div class="unit-signature-links">
+            <a id="cornell-link" href="http://www.cornell.edu/">Cornell University</a>
+            <a id="unit-link" href="http://www.library.cornell.edu">Cornell University Library</a>
+          </div>
+        </div>
+      </div>
+      <div class="search-links hidden-xs">
+        <ul>
+          <li><a href="http://www.library.cornell.edu/accessiblesearch/">Search Library</a></li>
+          <li><a href="http://www.cornell.edu/search/">Search Cornell</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  <a class="visible-xs" href="http://www.library.cornell.edu"><img src="/sites/all/themes/social/images/layout/cul-mobile.png" alt="Cornell University Library"></a>
+</div>
+
+<div class="navbar" role="navigation">
+  <div class="container">
+    <div class="row">
+      <div class="brand">
+        <a class="navbar-brand" href="/">Social@CUL</a>
+      </div>
+      <div class="social-types">
+        <ul class="nav nav-icons">
+          <li><a href="#"><i class="fa fa-pencil"></i></a></li>
+          <li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+          <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+          <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+          <li><a href="#"><i class="fa fa-youtube"></i></a></li>
+          <li><a href="#"><i class="fa fa-flickr"></i></a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="social-feed">
+  <div class="container-fluid">
+    <?php print render($page['feed']); ?>
+  </div>
+</div>
+
+<div class="container">
+  <footer>
+    <?php print render($page['footer']); ?>
+  </footer>
+</div> <!-- /container -->
+
+<?php print render($page['bottom']); ?>
