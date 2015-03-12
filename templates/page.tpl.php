fd5a45495cfdfcc943d4acf54340b4f9a826a8ed
Melissa Wallace <mhk33@cornell.edu>
2015-03-12 13:40:17 -0400

    clean up subpage

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -44,28 +44,24 @@
 
 <div class="main-content">
   <div class="container">
-    <div class="row">
-      <div class="main-text">
-        <?php print render($breadcrumb); ?>
-        <?php print render($page['highlighted']); ?>
-        <?php print $messages; ?>
-        <?php print render($tabs); ?>
-        <?php print render($page['help']); ?>
-        <?php if ($action_links): ?>
-          <ul class="action-links"><?php print render($action_links); ?></ul>
-        <?php endif; ?>
-        <?php if ($title): ?>
-          <h1><?php print $title; ?></h1>
-        <?php endif; ?>
-        <?php if(drupal_is_front_page()) {
-          unset($page['content']['system_main']['default_message']);
-        }?>
-        <?php if ( $is_front !== TRUE ) { 
-          print render($page['content']);
-        } ?>
-        <?php print render($page['homecontent']); ?>
-      </div>
-    </div>
+    <?php print render($breadcrumb); ?>
+    <?php print render($page['highlighted']); ?>
+    <?php print $messages; ?>
+    <?php print render($tabs); ?>
+    <?php print render($page['help']); ?>
+    <?php if ($action_links): ?>
+      <ul class="action-links"><?php print render($action_links); ?></ul>
+    <?php endif; ?>
+    <?php if ($title): ?>
+      <h1><?php print $title; ?></h1>
+    <?php endif; ?>
+    <?php if(drupal_is_front_page()) {
+      unset($page['content']['system_main']['default_message']);
+    }?>
+    <?php if ( $is_front !== TRUE ) { 
+      print render($page['content']);
+    } ?>
+    <?php print render($page['homecontent']); ?>
   </div>
 </div>
 
