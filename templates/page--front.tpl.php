7577c80cdda7d9ce28624584ac0355a7aca8714e
Melissa Wallace <mhk33@cornell.edu>
2015-03-12 11:46:30 -0400

    nav tool tips for social icons

--- templates/page--front.tpl.php
+++ templates/page--front.tpl.php
@@ -32,12 +32,12 @@
       </div>
       <div class="social-types">
         <ul class="nav nav-icons">
-          <li><a href="#"><i class="fa fa-pencil"></i></a></li>
-          <li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
-          <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-          <li><a href="#"><i class="fa fa-instagram"></i></a></li>
-          <li><a href="#"><i class="fa fa-youtube"></i></a></li>
-          <li><a href="#"><i class="fa fa-flickr"></i></a></li>
+          <li><a href="#" data-toggle="tooltip" data-placement="bottom" title="Blogs"><i class="fa fa-pencil"></i></a></li>
+          <li><a href="#" data-toggle="tooltip" data-placement="bottom" title="Facebook"><i class="fa fa-facebook-square"></i></a></li>
+          <li><a href="#" data-toggle="tooltip" data-placement="bottom" title="Twitter"><i class="fa fa-twitter"></i></a></li>
+          <li><a href="#" data-toggle="tooltip" data-placement="bottom" title="Instagram"><i class="fa fa-instagram"></i></a></li>
+          <li><a href="#" data-toggle="tooltip" data-placement="bottom" title="YouTube"><i class="fa fa-youtube"></i></a></li>
+          <li><a href="#" data-toggle="tooltip" data-placement="bottom" title="Flickr"><i class="fa fa-flickr"></i></a></li>
         </ul>
       </div>
     </div>
